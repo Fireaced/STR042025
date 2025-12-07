b2e311c8d00f680fdab87709c31ca93d381218f5
KinzCheetah24 <alvarete865@gmail.com>
2025-12-07 18:42:30 +0100

    fix: minimal code

--- interrupt_handler.adb
+++ interrupt_handler.adb
@@ -1,13 +1,7 @@
-with Ada.Interrupts;
-with Ada.Interrupts.Names;
 with Ada.Text_IO; use Ada.Text_IO;
-with FSS_Interfaces; use FSS_Interfaces;
 
 package body Interrupt_Handler is
 
-   ---------------------------
-   -- Mode manager body
-   ---------------------------
    protected body Mode_Manager_Type is
       procedure Switch_Mode is
       begin
@@ -22,41 +16,5 @@ package body Interrupt_Handler is
          return Current;
       end Current_Mode;
    end Mode_Manager_Type;
-
-   ---------------------------
-   -- Interrupt handler body
-   ---------------------------
-   protected body Interrupts is
-
-      procedure Button_Handler is
-      begin
-         Pending := True;         -- abre barrera
-         Mode_Manager.Switch_Mode;
-      end Button_Handler;
-
-      entry Wait_Event when Pending is
-      begin
-         Pending := False;        -- cierra barrera
-      end Wait_Event;
-
-   end Interrupts;
-
-   -------------------------
-   -- Sporadic task body --
-   -------------------------
-   task body Sporadic_Task is
-   begin
-      loop
-         Interrupts.Wait_Event;  -- espera interrupción
-
-         Put_Line ("Interrupción recibida. Modo actual: "
-                   & Mode_Manager.Current_Mode'Image);
-      end loop;
-   end Sporadic_Task;
-
-   procedure Initialize is
-   begin
-      null;
-   end Initialize;
 
 end Interrupt_Handler;
--- interrupt_handler.ads
+++ interrupt_handler.ads
@@ -1,13 +1,7 @@
-with Ada.Interrupts;
-with Ada.Interrupts.Names;
-
 package Interrupt_Handler is
 
    type Mode_Type is (Manual, Automatic);
 
-   ---------------------------------------
-   -- Protected mode manager TYPE
-   ---------------------------------------
    protected Mode_Manager_Type is
       procedure Switch_Mode;
       function Current_Mode return Mode_Type;
@@ -15,35 +9,6 @@ package Interrupt_Handler is
       Current : Mode_Type := Manual;
    end Mode_Manager_Type;
 
-   ---------------------------------------
-   -- Protected mode manager OBJECT
-   ---------------------------------------
    Mode_Manager : Mode_Manager_Type;
-
-   ---------------------------------------
-   -- Objeto protegido de interrupciones
-   ---------------------------------------
-   protected Interrupts is
-      pragma Priority (10);
-
-      procedure Button_Handler;
-      pragma Attach_Handler
-        (Button_Handler, Ada.Interrupts.Names.External_Interrupt_2);
-
-      entry Wait_Event;
-   private
-      Pending : Boolean := False;
-   end Interrupts;
-
-   Interrupts : Interrupts;
-
-   ---------------------------------------
-   -- Tarea esporádica
-   ---------------------------------------
-   task Sporadic_Task is
-      pragma Priority (5);
-   end Sporadic_Task;
-
-   procedure Initialize;
 
 end Interrupt_Handler;
